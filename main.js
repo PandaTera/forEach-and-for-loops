9238685b4a32f6da52d2c776cf1cf6e9b83cb022
PandaTera <47454997+PandaTera@users.noreply.github.com>
2020-04-04 20:31:37 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -22,19 +22,41 @@
 
 /*SECOND SOLUTION*/
 
-const btn = document.querySelector('button');
-const ul = document.querySelector('ul');
-const li = document.querySelectorAll('li');
+// const btn = document.querySelector('button');
+// const ul = document.querySelector('ul');
+// const li = document.querySelectorAll('li');
+// let size = 10;
+
+
+
+// function enlarge(){
+//     size++;
+//     ul.style.display ='block';
+// li.forEach((i) =>{
+//    i.style.fontSize = size + 'px';
+// })
+// }
+
+// btn.addEventListener('click', enlarge)
+
+/*THIRD SOLUTION - ADDED COLOR OPTIONS*/
+const btn = document.querySelector('button')
+const liList = document.querySelectorAll('li');
 let size = 10;
-
-
-
-function enlarge(){
+let color = ["orangered","blue"]
+btn.addEventListener('click', function(){
     size++;
-    ul.style.display ='block';
-li.forEach((i) =>{
-   i.style.fontSize = size + 'px';
-})
-}
 
-btn.addEventListener('click', enlarge)
+for(i=0; i < liList.length; i++){
+    liList[i].style.fontSize = size + 'px';
+    liList[i].style.display = "block";
+    if(i%2 ==0){
+        liList[i].style.color = color[0];
+    }else
+    {liList[i].style.color = color[1];}
+    
+}   
+
+
+       console.log('ok')
+})
